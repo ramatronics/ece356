f816f224e29dd97c4b0ba3a9711e3c1d777a0bdb
Ramie Raufdeen <me@ramie.ca>
2016-11-09 09:47:07 -0500

    transactions

---
+++ lab3/part3a.sql
@@ -0,0 +1,16 @@
+SET autocommit=0;
+
+SELECT salary FROM Employee WHERE empID = 66;
+
+/*CASE 1*/
+START TRANSACTION;
+UPDATE Employee SET salary = salary + 100000 WHERE empID = 66;
+ROLLBACK;
+
+SELECT salary FROM Employee WHERE empID = 66;
+
+/*CASE 2*/
+START TRANSACTION;
+UPDATE Employee SET salary = salary + 100000 WHERE empID = 66;
+COMMIT;
+SELECT salary FROM Employee WHERE empID = 66;
