6cefbe31e1d3632f475ad1a8a5aeb47c61b5aa4a
Ramie Raufdeen <me@ramie.ca>
2016-11-09 09:30:38 -0500

    DOIN THE LAB

--- lab3/part1.sql
+++ lab3/part1.sql
@@ -1,7 +1,6 @@
 CREATE INDEX salary_idx on Employee(salary) using BTREE;
 CREATE INDEX dept_idx on Employee(deptID) using HASH;
 
-
 SELECT * FROM employee
 	WHERE salary >= 30000 AND salary <= 40000;
 
--- lab3/part1c.sql
+++ lab3/part1c.sql
@@ -1,17 +1,3 @@
-SELECT * FROM employee
-	WHERE salary >= 30000 AND salary <= 40000;
-
-SELECT d.deptid,
-    d.deptname,
-    d.location,
-    e.empid,
-    e.empname,
-    e.job,
-    e.salary
-FROM department d
-	LEFT JOIN employee e
-	ON d.deptid = e.deptid;
-
 EXPLAIN SELECT * FROM employee
 			WHERE salary >= 30000 AND salary <= 40000;
 
